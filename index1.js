ea62c59248b1ae3b3418352a37e42c5b18bd49e4
sid3github <siddharthpadwal3@gmail.com>
2018-07-27 19:56:39 +0530

    program for stack using class

---
+++ index1.js
@@ -0,0 +1,60 @@
+class Stack {
+    constructor(size, items) {
+        this.size = size;
+        this.items = items;
+    };
+
+    push(element) {
+        this.items.push(element);
+        this.size++;
+    };
+
+    isEmpty(){
+        if(this.size === 0){
+            return 'the stack empty';
+        }
+    };
+
+    pop(){
+        if(this.isEmpty()){
+            return 'the stack empty';
+        }else {
+            this.items.pop();
+            this.size--;
+        }
+    };
+
+    search(element){
+        if(this.isEmpty()){
+            return 'the stack empty';
+        }else{
+            for(let i = 0; i < this.items.length; i++) {
+                if (element === this.items[i]) {
+                    return 'element found!';
+                }
+            }
+            return 'element not found!';
+        }
+    };
+}
+
+let Data = new Stack(0 , []);
+Data.push(100);
+Data.push(200);
+Data.push(300);
+Data.push(400);
+// console.log(Data);
+Data.pop();
+console.log(Data);
+console.log(Data.search(200));
+console.log(Data.search(5));
+
+
+
+
+
+
+
+
+
+
